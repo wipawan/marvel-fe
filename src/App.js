e20dc18eb0c178321ead294b896b0bcf25fba97e
wipawan <mew.amornchairattanakul@datadoghq.com>
2024-08-13 00:25:52 +0800

    update marvel frontend app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
+import FormComponent from "./FormComponent";
+import ActionPage from "./ActionPage";
 
 function App() {
+  const [username, setUsername] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={<FormComponent setUsername={setUsername} />}
+          />
+          <Route path="/actions" element={<ActionPage username={username} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
